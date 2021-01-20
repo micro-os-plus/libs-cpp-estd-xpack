27b81699f047239a928b6bca7b0a8b4cb53cc519
Liviu Ionescu <ilg@livius.net>
2021-01-20 22:26:55 +0200

    add meta/xpack-helper.cmake

---
+++ meta/xpack-helper.cmake
@@ -0,0 +1,43 @@
+#
+# This file is part of the µOS++ distribution.
+#   (https://github.com/micro-os-plus)
+# Copyright (c) 2021 Liviu Ionescu
+#
+# This Source Code Form is subject to the terms of the MIT License.
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/MIT/.
+#
+# -----------------------------------------------------------------------------
+
+message(STATUS "Including micro-os-plus-libs-cpp-estd...")
+
+function(target_sources_micro_os_plus_libs_cpp_estd target)
+
+  get_filename_component(PARENT_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+
+  target_sources(
+    ${target}
+
+    PRIVATE
+    ${PARENT_DIR}/src/chrono.cpp
+    ${PARENT_DIR}/src/condition-variable.cpp
+    ${PARENT_DIR}/src/memory-resource.cpp
+    ${PARENT_DIR}/src/mutex.cpp
+    ${PARENT_DIR}/src/new.cpp
+    ${PARENT_DIR}/src/thread.cpp
+  )
+endfunction()
+
+function(target_include_directories_micro_os_plus_libs_cpp_estd target)
+
+  get_filename_component(PARENT_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+
+  target_include_directories(
+    ${target}
+
+    PUBLIC
+      ${PARENT_DIR}/include
+  )
+endfunction()
+
+# -----------------------------------------------------------------------------
